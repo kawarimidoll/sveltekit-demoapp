f08372c3cdae132c2fb99496652c1e9bb0b8a860
kawarimidoll <kawarimidoll+git@gmail.com>
2024-11-20 11:03:06 +1100

    feat: add timestamp columns

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,10 +1,17 @@
 import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
 
+const timestampOptions = { withTimezone: true, mode: 'date', precision: 3 };
+const timestamps = {
+  createdAt: timestamp(timestampOptions).notNull().$default(() => new Date()),
+  updatedAt: timestamp(timestampOptions).notNull().$onUpdate(() => new Date()),
+};
+
 export const user = pgTable('user', {
   id: text().primaryKey(),
   age: integer(),
   username: text().notNull().unique(),
   passwordHash: text().notNull(),
+  ...timestamps,
 });
 
 export const session = pgTable('session', {
