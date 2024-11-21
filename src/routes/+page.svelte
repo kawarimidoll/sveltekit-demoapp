62deecd8493e666297f03a7ff9b6bfa5b41b923f
kawarimidoll <kawarimidoll+git@gmail.com>
2024-11-21 15:31:24 +1100

    feat: remove paraglide demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang='ts'>
+  import * as m from '$lib/paraglide/messages.js';
+</script>
+
+<h1>{m.hello_world()}</h1>
 <p>Visit <a href='https://svelte.dev/docs/kit'>svelte.dev/docs/kit</a> to read the documentation</p>
 
-<h2>demo</h2>
 <div>
-  <a href='/lucia'>lucia</a>
-  <a href='/paraglide'>paraglide</a>
+  <a href='/lucia'>lucia demo</a>
 </div>
