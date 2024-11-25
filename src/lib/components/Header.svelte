336538e7f250f666adcc21c05728dfe81cefda07
kawarimidoll <kawarimidoll+git@gmail.com>
2024-11-25 11:27:59 +1100

    feat: add admin top

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -10,11 +10,17 @@
     const localisedPath = i18n.resolveRoute(canonicalPath, newLanguage);
     goto(localisedPath);
   }
+
+  const isAdmin = i18n.route($page.url.pathname).startsWith('/admin');
 </script>
 
 <header class='bg-background/75 border-border sticky top-0 z-50 border-b backdrop-blur -mb-px'>
   <div class='max-w-8xl mx-auto h-16 flex items-center justify-between container'>
-    <a href='/'>TOP</a>
+    {#if isAdmin}
+      <a href='/admin'>Admin Top</a>
+    {:else}
+      <a href='/'>Top</a>
+    {/if}
 
     {#each availableLanguageTags as languageTag}
       <button onclick={() => switchToLanguage(languageTag)}>
