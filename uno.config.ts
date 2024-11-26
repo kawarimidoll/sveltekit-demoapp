dddd18ad397d032d1b16cb743d82e71ba1e606ab
kawarimidoll <kawarimidoll+git@gmail.com>
2024-11-26 17:28:19 +1100

    chore: setup unocss and daisyui

--- uno.config.ts
+++ uno.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig, presetAttributify, presetUno } from 'unocss';
+import presetDaisy from '@vsilk/unocss-preset-daisyui';
+import {
+  defineConfig,
+  presetAttributify,
+  presetUno,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss';
+import presetAnimations from 'unocss-preset-animations';
 
 export default defineConfig({
+  transformers: [
+    transformerDirectives(),
+    transformerVariantGroup(),
+  ],
   presets: [
     presetUno(),
     presetAttributify({ prefix: 'uno-', prefixedOnly: true }),
+    presetAnimations(),
+    presetDaisy(),
   ],
 });
