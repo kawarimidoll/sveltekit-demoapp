8c72fd891d03cf192927d31d872a5340fbfb4ee4
kawarimidoll <kawarimidoll+git@gmail.com>
2024-11-27 13:53:30 +1100

    feat: forward link with closing modal

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,8 @@
 <script lang='ts'>
+  import { goto } from '$app/navigation';
   import { page } from '$app/stores';
   import { i18n } from '$lib/i18n';
+  import { languageTag } from '$lib/paraglide/runtime';
   import LangSelect from './LangSelect.svelte';
 
   const isAdmin = i18n.route($page.url.pathname).startsWith('/admin');
@@ -10,7 +12,19 @@
 
   const { form } = $props();
 
-  let isOpen = $state(true);
+  let isOpen = $state(false);
+  function showModal() {
+    isOpen = true;
+  }
+  function hideModal(forward?: string) {
+    isOpen = false;
+
+    if (forward) {
+      const canonicalPath = i18n.route(forward);
+      const localisedPath = i18n.resolveRoute(canonicalPath, languageTag());
+      goto(localisedPath);
+    }
+  }
 </script>
 
 <header class='bg-background/75 border-border sticky top-0 z-50 border-b backdrop-blur -mb-px'>
@@ -24,7 +38,7 @@
     <LangSelect />
 
     <div class='grid size-10 place-items-center border rounded-full'>
-      <button aria-label='user-login' class='bg-transparent' onclick={() => (isOpen = true)}>
+      <button aria-label='user-login' class='bg-transparent' onclick={showModal}>
         <div class='i-octicon-person-fill-16 size-6'></div>
       </button>
     </div>
@@ -47,15 +61,14 @@
       </label>
       <button class='w-full btn btn-primary'>{m.login()}</button>
       <p style='color: red'>{form?.message ?? ''}</p>
-      <!-- <button class='btn w-full' formaction='?/register'>{m.register()}</button> -->
     </form>
     <div class='divider'>OR</div>
     <div class='flex justify-evenly text-right text-sm'>
-      <p><a class='link-primary link' href='/forget_password'>Forget your password?</a></p>
-      <p><a class='link-primary link' href='/register'>Don't have an account?</a></p>
+      <button class='btn btn-link' onclick={() => hideModal('/forgot-password')}>Forgot your password?</button>
+      <button class='btn btn-link' onclick={() => hideModal('/register')}>Don't have an account?</button>
     </div>
   </div>
   <form method='dialog' class='modal-backdrop'>
-    <button class='cursor-default opacity-50' onclick={() => (isOpen = false)}>close</button>
+    <button class='cursor-default opacity-50' onclick={() => hideModal()}>close</button>
   </form>
 </dialog>
