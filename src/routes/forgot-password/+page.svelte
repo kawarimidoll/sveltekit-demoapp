13957325c34bfd73b8f9b0ddf08652d76266af65
kawarimidoll <kawarimidoll+git@gmail.com>
2024-11-27 13:54:01 +1100

    feat: add register and forgot-password pages

---
+++ src/routes/forgot-password/+page.svelte
@@ -0,0 +1,19 @@
+<script lang='ts'>
+  import type { ActionData } from './$types';
+  import { enhance } from '$app/forms';
+  import * as m from '$lib/paraglide/messages.js';
+
+  const { form }: { form: ActionData } = $props();
+
+</script>
+
+<h1>Reset Password</h1>
+<p>Enter your email address and we'll send you a link to reset your password.</p>
+<form method='post' action='?/login' use:enhance>
+  <label class='flex items-center gap-2 input input-bordered'>
+    <div class='i-octicon-mail-16'></div>
+    <input type='text' name='username' placeholder='username' class='grow' />
+  </label>
+  <button formaction='?/register'>{m.register()}</button>
+</form>
+<p style='color: red'>{form?.message ?? ''}</p>
---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,22 @@
+<script lang='ts'>
+  import type { ActionData } from './$types';
+  import { enhance } from '$app/forms';
+  import * as m from '$lib/paraglide/messages.js';
+
+  const { form }: { form: ActionData } = $props();
+
+</script>
+
+<h1>{m.register()}</h1>
+<form method='post' action='?/login' use:enhance>
+  <label>
+    Username
+    <input name='username' />
+  </label>
+  <label>
+    Password
+    <input type='password' name='password' />
+  </label>
+  <button formaction='?/register'>{m.register()}</button>
+</form>
+<p style='color: red'>{form?.message ?? ''}</p>
