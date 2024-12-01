23b01e0a438b412cc2055102aa014c60e3f0929d
kawarimidoll <kawarimidoll+git@gmail.com>
2024-12-01 21:53:21 +1100

    fix: use i18nRedirect instead of redirect in admin layout.server

--- src/routes/admin/+layout.server.ts
+++ src/routes/admin/+layout.server.ts
@@ -1,11 +1,14 @@
-import type { PageServerLoad } from './$types';
-import { redirect } from '@sveltejs/kit';
+import type { LayoutServerLoad } from './$types';
+import { i18n } from '$lib/i18n';
+import { i18nRedirect } from '$lib/server/utils';
 
-export const load: PageServerLoad = async (event) => {
+export const load: LayoutServerLoad = async (event) => {
   const { locals, url } = event;
   if (!locals.admin) {
-    if (url.pathname !== '/admin/login') {
-      return redirect(302, `/admin/login?redirectTo=${url.pathname}`);
+    const canonicalPath = i18n.route(url.pathname);
+    if (canonicalPath !== '/admin/login') {
+      // FIXME: i18nRedirect removes the query string
+      return i18nRedirect(url, `/admin/login?redirectTo=${url.pathname}`);
     }
     return;
   }
