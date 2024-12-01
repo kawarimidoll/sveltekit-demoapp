770f6713bfb49f1c73229960f08ca1757e6259c9
kawarimidoll <kawarimidoll+git@gmail.com>
2024-12-01 17:57:37 +1100

    feat: improve page design

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,23 +1,21 @@
 <script lang='ts'>
   import type { ActionData } from './$types';
   import { enhance } from '$app/forms';
+  import Input from '$lib/components/Input.svelte';
   import * as m from '$lib/paraglide/messages.js';
 
   const { form }: { form: ActionData } = $props();
 
 </script>
 
-<h1>{m.login()}/{m.register()}</h1>
-<form method='post' action='?/login' use:enhance>
-  <label>
-    Username
-    <input name='username' />
-  </label>
-  <label>
-    Password
-    <input type='password' name='password' />
-  </label>
-  <button>{m.login()}</button>
-  <button formaction='?/register'>{m.register()}</button>
-</form>
-<p style='color: red'>{form?.message ?? ''}</p>
+<section class='m-auto w-full sm:max-w-md'>
+  <form class='space-y-2' method='post' action='?/login' use:enhance>
+    <div class='mb-4 text-center'>
+      <h3 class='text-lg font-bold'>{m.login()}</h3>
+    </div>
+    <Input type='text' name='username' placeholder='username' icon='i-octicon-person-fill-16' />
+    <Input type='password' name='password' placeholder='password' icon='i-octicon-key' />
+    <button class='w-full btn btn-primary'>{m.login()}</button>
+    <p style='color: red'>{form?.message ?? ''}</p>
+  </form>
+</section>
