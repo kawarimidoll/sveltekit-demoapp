9780020bc205e0567c917f8edb416b43f14bc2f2
kawarimidoll <kawarimidoll+git@gmail.com>
2024-12-02 15:19:55 +1100

    breaking: delete all assets

--- src/lib/server/utils.ts
+++
@@ -1,7 +0,0 @@
-import { i18n } from '$lib/i18n';
-import { redirect } from '@sveltejs/kit';
-
-export function i18nRedirect(from: URL, to: string) {
-  const lang = i18n.getLanguageFromUrl(from);
-  return redirect(302, i18n.resolveRoute(to, lang));
-}
