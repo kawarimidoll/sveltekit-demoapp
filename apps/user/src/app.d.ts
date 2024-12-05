6b703fd22939e7449184395368dc8434f9cbb0d1
kawarimidoll <kawarimidoll+git@gmail.com>
2024-12-05 21:35:18 +1100

    fix: fix type definition

--- apps/user/src/app.d.ts
+++ apps/user/src/app.d.ts
@@ -1,10 +1,15 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+import type { AvailableLanguageTag } from '$lib/paraglide/runtime';
+import type { SessionValidationResult } from '$lib/server/auth';
+import type { ParaglideLocals } from '@inlang/paraglide-sveltekit';
+
 declare global {
   namespace App {
     interface Locals {
-      user: import('$lib/server/auth').SessionValidationResult['user'];
-      session: import('$lib/server/auth').SessionValidationResult['userSession'];
+      user: SessionValidationResult['user'];
+      session: SessionValidationResult['userSession'];
+      paraglide: ParaglideLocals<AvailableLanguageTag>;
     }
   }
 }
