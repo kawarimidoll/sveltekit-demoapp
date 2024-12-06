36b13ff6ddab27f47ac616df869645a103bdae27
kawarimidoll <kawarimidoll+git@gmail.com>
2024-12-06 15:01:34 +1100

    feat: add Input component

---
+++ apps/user/src/lib/components/Input.svelte
@@ -0,0 +1,11 @@
+<script lang='ts'>
+  // https://daisyui.com/components/input/#text-input-with-icon-inside
+  const { type = 'text', icon, ...attr } = $props();
+</script>
+
+<label class='flex items-center gap-2 input input-bordered'>
+  {#if icon}
+    <div class={icon}></div>
+  {/if}
+  <input {type} {...attr} class='grow bg-clip-text' />
+</label>
