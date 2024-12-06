6c020269d7b4f8369da3f53f24d497f64a91bf8c
kawarimidoll <kawarimidoll+git@gmail.com>
2024-12-06 14:50:34 +1100

    feat: style Nav component

--- apps/user/src/lib/components/Nav.svelte
+++ apps/user/src/lib/components/Nav.svelte
@@ -4,7 +4,6 @@
   import { goto } from '$app/navigation';
   import { page } from '$app/stores';
   import { i18n } from '$lib/i18n';
-  import * as m from '$lib/paraglide/messages.js';
   import { availableLanguageTags, languageTag } from '$lib/paraglide/runtime';
   import { AvatarBeam } from 'svelte-boring-avatars';
 
@@ -17,25 +16,60 @@
   const { data } = $props();
 </script>
 
-<div>
-  <a href='/'>top</a>
-</div>
-<div>{m.hello_world({ name: data.user?.username || 'SvelteKit User' })}</div>
-<div>
-  {#each availableLanguageTags as item}
-    <button onclick={() => switchToLanguage(item)}>
-      {item === languageTag() ? '*' : ''}{item}
-    </button>
-  {/each}
-</div>
-{#if data.user != null}
-  <a href='/mypage'>mypage</a>
-  <a href='/settings'>settings</a>
-  <AvatarBeam name={data.user.id} />
+<div class='bg-base-100 navbar'>
+  <div class='flex-1'>
+    <a class='text-xl btn btn-ghost' href='/'>
+      Top
+    </a>
+  </div>
 
-  <form method='post' action='/logout' use:enhance>
-    <button>Sign out</button>
-  </form>
-{:else}
-  <a href='/login'>login</a>
-{/if}
+  <div class='flex-none gap-2'>
+    <div class='form-control'>
+      <input type='text' placeholder='[WIP] Search' class='w-24 input input-bordered md:w-auto' />
+    </div>
+
+    <div class='dropdown dropdown-end'>
+      <div tabindex='0' role='button' class='m-1 btn btn-circle btn-ghost'>
+        <span class='i-fluent-translate-16-regular size-6'></span>
+      </div>
+      <div class='bg-base-100 rounded-box z-[1] w-26 p-2 shadow menu dropdown-content'>
+        <ul class='gap-1 menu menu-sm'>
+          {#each availableLanguageTags as item}
+            <li>
+              <button
+                class='btn btn-sm'
+                class:btn-neutral={item === languageTag()}
+                class:btn-outline={item !== languageTag()}
+                onclick={() => switchToLanguage(item)}
+              >
+                {item}
+              </button>
+            </li>
+          {/each}
+        </ul>
+      </div>
+    </div>
+
+    <div class='dropdown dropdown-end'>
+      <div tabindex='0' role='button' class='m-1 btn btn-circle btn-ghost'>
+        {#if data.user != null}
+          <AvatarBeam name={data.user.id} />
+        {:else}
+          <div class='size-40'>L</div>
+        {/if}
+      </div>
+      <div class='rounded-box bg-base-100 z-[1] w-26 p-2 shadow menu dropdown-content'>
+        {#if data.user != null}
+          <a class='link' href='/mypage'>mypage</a>
+          <a class='link' href='/settings'>settings</a>
+          <form method='post' action='/logout' use:enhance>
+            <button class='btn btn-link btn-sm'>Sign out</button>
+          </form>
+        {:else}
+          <a class='link' href='/login'>login</a>
+        {/if}
+      </div>
+    </div>
+
+  </div>
+</div>
