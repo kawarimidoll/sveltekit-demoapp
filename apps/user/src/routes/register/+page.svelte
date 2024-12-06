a0cc402ce1d25c2e1a91a4b07776f77db59c64da
kawarimidoll <kawarimidoll+git@gmail.com>
2024-12-06 15:27:17 +1100

    feat: style register page

--- apps/user/src/routes/register/+page.svelte
+++ apps/user/src/routes/register/+page.svelte
@@ -1,42 +1,42 @@
 <script lang='ts'>
   import type { ActionData, PageServerData } from './$types';
   import { enhance } from '$app/forms';
+  import { Input } from '$lib/components';
 
   const { data, form }: { data: PageServerData;form: ActionData } = $props();
 </script>
 
-<h1>Register</h1>
-<form method='post' action='?/register' use:enhance>
-  <label>
-    Email
+<section class='m-auto w-full sm:max-w-md'>
+  <form class='space-y-2' method='post' action='?/register' use:enhance>
+    <div class='mb-4 text-center'>
+      <h2>Register</h2>
+    </div>
     <input type='hidden' name='email' value={data.email} />
-    {data.email}
-  </label>
-  <label>
-    Verification code
-    <input type='text' name='code' required />
-  </label>
-  <label>
-    Password
-    <input type='password' name='password' required />
-  </label>
-  <button>Register</button>
-</form>
-<p style='color: red'>{form?.message ?? ''}</p>
+    <Input disabled type='email' value={data.email} icon='i-octicon-mail-16' />
+    <Input required type='text' name='code' placeholder='Verification code' icon='i-octicon-shield-lock-16' />
+    <Input required type='password' name='password' placeholder='password' icon='i-octicon-key' />
+    <button class='w-full btn btn-primary'>Register</button>
+    <p style='color: red'>{form?.message ?? ''}</p>
+  </form>
+  <div class='mt-4'>
+    Already have an account? <a href='/login'>Sign in</a>
+  </div>
+  <div>
+    <form method='post' action='/verify-email?/request_verify' use:enhance>
+      <input type='hidden' name='email' value={data.email} />
+      Code expired? <button>Resend code</button>
+    </form>
+  </div>
+  <div>
+    Use different email? <a href='/verify-email'>Request code again</a>
+  </div>
+</section>
 
-<div>
-  <a href='/verify-email'>Change email?</a>
-</div>
-<form method='post' action='/verify-email?/request_verify' use:enhance>
-  <input type='hidden' name='email' value={data.email} />
-  Code expired? <button>Resend code</button>
-</form>
-<div>
-  Already have an account? <a href='/login'>Sign in</a>
-</div>
-
-<div>
-  <p>What's 'Weak password'?</p>
-  <p>We validate your password strength in an anonymized form.</p>
-  <p><a href='https://haveibeenpwned.com/API/v2#SearchingPwnedPasswordsByRange'>The validation API</a>  is run by <a href='https://haveibeenpwned.com/'>Have I been Pwned</a>.</p>
+<div class='bg-base-200 collapse'>
+  <input type='checkbox' />
+  <div class='text-xl font-medium collapse-title'>What's <span class='italic'>Weak password</span> ?</div>
+  <div class='collapse-content'>
+    <p>We validate your password strength in an anonymized form.</p>
+    <p><a href='https://haveibeenpwned.com/API/v2#SearchingPwnedPasswordsByRange'>The validation API</a>  is run by <a href='https://haveibeenpwned.com/'>Have I been Pwned</a>.</p>
+  </div>
 </div>
