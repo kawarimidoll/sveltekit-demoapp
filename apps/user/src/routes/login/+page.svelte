48354efcadaec097dc9075df9b8d1200550fa9ac
kawarimidoll <kawarimidoll+git@gmail.com>
2024-12-06 15:01:49 +1100

    feat: style login page

--- apps/user/src/routes/login/+page.svelte
+++ apps/user/src/routes/login/+page.svelte
@@ -1,24 +1,22 @@
 <script lang='ts'>
   import type { ActionData } from './$types';
   import { enhance } from '$app/forms';
+  import { Input } from '$lib/components';
 
   const { form }: { form: ActionData } = $props();
 </script>
 
-<h1>Login</h1>
-<form method='post' action='?/login' use:enhance>
-  <label>
-    Email
-    <input type='email' name='email' required />
-  </label>
-  <label>
-    Password
-    <input type='password' name='password' required />
-  </label>
-  <button>Login</button>
-</form>
-<p style='color: red'>{form?.message ?? ''}</p>
-
-<div>
-  Don't have an account? <a href='/register'>Create account</a>
-</div>
+<section class='m-auto w-full sm:max-w-md'>
+  <form class='space-y-2' method='post' action='?/login' use:enhance>
+    <div class='mb-4 text-center'>
+      <h2>Login</h2>
+    </div>
+    <Input required type='email' name='email' placeholder='email' icon='i-octicon-mail-16' />
+    <Input required type='password' name='password' placeholder='password' icon='i-octicon-key' />
+    <button class='w-full btn btn-primary'>Login</button>
+    <p style='color: red'>{form?.message ?? ''}</p>
+  </form>
+  <div class='mt-4'>
+    Don't have an account? <a href='/register'>Create account</a>
+  </div>
+</section>
