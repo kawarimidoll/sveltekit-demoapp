f4a59fc731b57b7915001a3ce57a045c2011000c
kawarimidoll <kawarimidoll+git@gmail.com>
2024-12-08 22:15:03 +1100

    feat: copy auth from user to admin

--- apps/admin/src/lib/server/auth.ts
+++ apps/admin/src/lib/server/auth.ts
@@ -1,81 +1,80 @@
 import type { RequestEvent } from '@sveltejs/kit';
-import { db } from '$lib/server/db';
-import * as table from '$lib/server/db/schema';
-import { sha256 } from '@oslojs/crypto/sha2';
-import { encodeBase64url, encodeHexLowerCase } from '@oslojs/encoding';
+import { db } from '@shared/db';
+import * as table from '@shared/db/schema';
+import { deleteCookie, setCookie } from '@shared/logic/cookie';
+import { encodeSessionToken } from '@shared/logic/session-token';
 import { eq } from 'drizzle-orm';
 
+export { generateSessionToken } from '@shared/logic/session-token';
+
 const DAY_IN_MS = 1000 * 60 * 60 * 24;
+const EXPIRATION_DAYS = DAY_IN_MS * 30;
 
-export const sessionCookieName = 'auth-session';
+export const sessionCookieName = 'admin-auth-session';
 
-export function generateSessionToken() {
-  const bytes = crypto.getRandomValues(new Uint8Array(18));
-  const token = encodeBase64url(bytes);
-  return token;
+export function isAuth(event: RequestEvent) {
+  return event.locals.session && event.locals.admin;
 }
 
-export async function createSession(token: string, userId: string) {
-  const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
-  const session: table.Session = {
-    id: sessionId,
-    userId,
-    expiresAt: new Date(Date.now() + DAY_IN_MS * 30),
-  };
-  await db.insert(table.session).values(session);
+export async function createSession(token: string, adminId: string) {
+  const encodedToken = encodeSessionToken(token);
+  await db.insert(table.adminSession).values({
+    encodedToken,
+    adminId,
+    expiresAt: new Date(Date.now() + EXPIRATION_DAYS),
+  });
   return session;
 }
 
 export async function validateSessionToken(token: string) {
-  const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+  const encodedToken = encodeSessionToken(token);
   const [result] = await db
     .select({
-      // Adjust user table here to tweak returned data
-      user: { id: table.user.id, username: table.user.username },
-      session: table.session,
+      // Adjust admin table here to tweak returned data
+      admin: { id: table.admin.id, name: table.admin.name, email: table.admin.email },
+      session: table.adminSession,
     })
-    .from(table.session)
-    .innerJoin(table.user, eq(table.session.userId, table.user.id))
-    .where(eq(table.session.id, sessionId));
+    .from(table.adminSession)
+    .innerJoin(table.admin, eq(table.adminSession.adminId, table.admin.id))
+    .where(eq(table.adminSession.encodedToken, encodedToken));
 
   if (!result) {
-    return { session: null, user: null };
+    return { session: null, admin: null };
   }
-  const { session, user } = result;
+  const { session, admin } = result;
 
   const sessionExpired = Date.now() >= session.expiresAt.getTime();
   if (sessionExpired) {
-    await db.delete(table.session).where(eq(table.session.id, session.id));
-    return { session: null, user: null };
+    await db.delete(table.adminSession).where(eq(table.adminSession.encodedToken, encodedToken));
+    return { session: null, admin: null };
   }
 
-  const renewSession = Date.now() >= session.expiresAt.getTime() - DAY_IN_MS * 15;
+  const renewSession = Date.now() >= session.expiresAt.getTime() - EXPIRATION_DAYS / 2;
   if (renewSession) {
-    session.expiresAt = new Date(Date.now() + DAY_IN_MS * 30);
+    session.expiresAt = new Date(Date.now() + EXPIRATION_DAYS);
     await db
-      .update(table.session)
+      .update(table.adminSession)
       .set({ expiresAt: session.expiresAt })
-      .where(eq(table.session.id, session.id));
+      .where(eq(table.adminSession.encodedToken, encodedToken));
   }
 
-  return { session, user };
+  return { session, admin };
 }
 
 export type SessionValidationResult = Awaited<ReturnType<typeof validateSessionToken>>;
 
-export async function invalidateSession(sessionId: string) {
-  await db.delete(table.session).where(eq(table.session.id, sessionId));
+export async function invalidateSession(encodedToken: string) {
+  await db.delete(table.adminSession).where(eq(table.adminSession.encodedToken, encodedToken));
+}
+
+export async function invalidateAllSession(adminId: string) {
+  await db.delete(table.adminSession).where(eq(table.adminSession.adminId, adminId));
 }
 
 export function setSessionTokenCookie(event: RequestEvent, token: string, expiresAt: Date) {
-  event.cookies.set(sessionCookieName, token, {
-    expires: expiresAt,
-    path: '/',
-  });
+  setCookie(event, sessionCookieName, token, expiresAt);
 }
 
 export function deleteSessionTokenCookie(event: RequestEvent) {
-  event.cookies.delete(sessionCookieName, {
-    path: '/',
-  });
+  deleteCookie(event, sessionCookieName);
 }
