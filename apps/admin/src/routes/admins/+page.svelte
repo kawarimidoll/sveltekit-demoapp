44d58092d463b14bd7e00dbd7ac0e35c9b4af136
kawarimidoll <kawarimidoll+git@gmail.com>
2024-12-09 15:14:42 +1100

    refactor: add Table component

--- apps/admin/src/routes/admins/+page.svelte
+++ apps/admin/src/routes/admins/+page.svelte
@@ -1,6 +1,7 @@
 <script lang='ts'>
   import type { PageServerData } from './$types';
   import { format } from '@std/datetime';
+  import Table from './Table.svelte';
   // import { enhance } from '$app/forms';
   // import { Input } from '@shared/components';
 
@@ -8,39 +9,33 @@
 </script>
 
 <section class='m-auto w-full'>
-  <div class='mb-4'>
-    <h2>Admins</h2>
-  </div>
-  <div class='overflow-x-auto'>
-    <table class='table divide-y divide-gray-200 dark:divide-neutral-700'>
-      <thead class=''>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Level</th>
-          <th>Status</th>
-          <th>Created at</th>
-          <th>Action</th>
+  <Table title='Admins'>
+    {#snippet thead()}
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Level</th>
+        <th>Status</th>
+        <th>Created at</th>
+        <th>Action</th>
+      </tr>
+    {/snippet}
+    {#snippet tbody()}
+      {#each data.admins as admin}
+        <tr class='hover:bg-gray-100 dark:hover:bg-neutral-700'>
+          <td>{admin.id}</td>
+          <td>{admin.name}</td>
+          <td>{admin.email}</td>
+          <td>{admin.level}</td>
+          <td>{admin.status}</td>
+          <td>{format(admin.createdAt, 'yyyy-MM-dd HH:mm:ss')}</td>
+          <td></td>
         </tr>
-      </thead>
-      <tbody class='divide-y divide-gray-200 dark:divide-neutral-700'>
-        {#each data.admins as admin}
-          <tr class='hover:bg-gray-100 dark:hover:bg-neutral-700'>
-            <td>{admin.id}</td>
-            <td>{admin.name}</td>
-            <td>{admin.email}</td>
-            <td>{admin.level}</td>
-            <td>{admin.status}</td>
-            <td>{format(admin.createdAt, 'yyyy-MM-dd HH:mm:ss')}</td>
-            <td></td>
-          </tr>
-        {/each}
-      </tbody>
-    </table>
-  </div>
+      {/each}
+    {/snippet}
+  </Table>
   <div>
     'Create at' is local time
   </div>
-
 </section>
---
+++ apps/admin/src/routes/admins/Table.svelte
@@ -0,0 +1,17 @@
+<script lang='ts'>
+  const { title, thead, tbody } = $props();
+</script>
+
+<div class='mb-4'>
+  <h2>{title}</h2>
+</div>
+<div class='overflow-x-auto'>
+  <table class='table divide-y divide-gray-200 dark:divide-neutral-700'>
+    <thead class=''>
+      {@render thead()}
+    </thead>
+    <tbody class='divide-y divide-gray-200 dark:divide-neutral-700'>
+      {@render tbody()}
+    </tbody>
+  </table>
+</div>
