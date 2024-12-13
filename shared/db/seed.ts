4f2932c1861c61dab3f7c21ee5c5c4e3aff56014
kawarimidoll <kawarimidoll+git@gmail.com>
2024-12-13 15:03:56 +1100

    chore: improve seed

--- shared/db/seed.ts
+++ shared/db/seed.ts
@@ -1,9 +1,15 @@
 import { rangeIterator } from '@hugoalh/range-iterator';
 import { hashPassword } from '@shared/logic/password';
+import { sample } from '@std/collections/sample';
 import { reset } from 'drizzle-seed';
 import { db } from './index';
 import list from './ndc_list.json';
+
 import * as schema from './schema';
+
+function nums(max: number) {
+  return Array.from(rangeIterator(1, max));
+}
 
 async function insertGenre(code: string, description: string) {
   await db
@@ -42,14 +48,38 @@ async function main() {
     ...Object.entries(list)
       .map(([code, description]) => insertGenre(code, description)),
     insertUser('hello@example.com'),
-    ...Array.from(rangeIterator(1, 40))
-      .map(e => insertUser(`user${e}@example.com`)),
+    ...nums(40).map(n => insertUser(`user${n}@example.com`)),
     insertAdmin('admin@example.com', 'super'),
-    ...Array.from(rangeIterator(1, 20))
-      .map(e => insertAdmin(`member${e}@example.com`, 'normal')),
-    ...Array.from(rangeIterator(1, 20))
-      .map(e => insertAdmin(`supporter${e}@example.com`, 'limited')),
+    ...nums(20).map(n => insertAdmin(`member${n}@example.com`, 'normal')),
+    ...nums(20).map(n => insertAdmin(`supporter${n}@example.com`, 'limited')),
   ]);
+
+  const publishers = await db.insert(schema.publisher)
+    .values(nums(10).map(n => ({ name: `publisher${n}` })))
+    .returning({ id: schema.publisher.id });
+  const authors = await db.insert(schema.author)
+    .values(nums(10).map(n => ({ name: `author${n}` })))
+    .returning({ id: schema.author.id });
+
+  // set 10 years ago
+  const pastDate = new Date();
+  pastDate.setFullYear(pastDate.getFullYear() - 10);
+
+  const books = await db.insert(schema.book)
+    .values(nums(20).map(n => ({
+      title: `book${n}`,
+      publishDate: pastDate,
+      publisherId: sample(publishers)!.id,
+    })))
+    .returning({ id: schema.book.id });
+
+  await db.insert(schema.bookAuthor)
+    .values(books.map(b => ({
+      bookId: b.id,
+      authorId: sample(authors)!.id,
+    })));
+
+  console.log({ publishers, authors, books });
 
   console.log('seed completed');
 }
