513564304d0a97c2e0bedcb37d77d10464edb295
kawarimidoll <kawarimidoll+git@gmail.com>
2024-12-22 11:18:38 +1100

    feat: add authors page

---
+++ apps/admin/src/routes/authors/+page.server.ts
@@ -0,0 +1,98 @@
+import type { RequestEvent } from '@sveltejs/kit';
+import type { SQL } from 'drizzle-orm';
+import type { PageServerLoad } from './$types';
+import { db, schema } from '@shared/db';
+import { genPagination } from '@shared/logic/pagination';
+import { getOptionsParam, getPositiveIntParam } from '@shared/logic/params';
+import { fail } from '@sveltejs/kit';
+import { and, eq, ilike, or } from 'drizzle-orm';
+
+export const load: PageServerLoad = async (event: RequestEvent) => {
+  const params = new URLSearchParams(event.url.search);
+
+  const page = getPositiveIntParam(params, 'page');
+
+  const search = params.get('search') || '';
+
+  const sort = getOptionsParam(params, 'sort', ['name']);
+  const order = getOptionsParam(params, 'order', ['asc', 'desc']);
+
+  const filters: SQL[] = [];
+  if (search) {
+    filters.push(or(
+      ilike(schema.author.name, `%${search}%`),
+    )!,
+    );
+  }
+
+  const per = 10;
+
+  const authors = await db.query.author.findMany({
+    where: and(...filters),
+    orderBy: (columns, { asc, desc }) =>
+      [order === 'desc' ? desc(columns[sort]) : asc(columns[sort])],
+    limit: per,
+    offset: (page - 1) * per,
+    extras: (author, { sql }) => ({
+      count: (sql<number>`count(${author.id}) over()`).as('count'),
+    }),
+  });
+  const count = authors[0]?.count || 0;
+  const maxPage = Math.ceil(count / per);
+
+  const pagination = genPagination(event.url, page, maxPage);
+
+  return {
+    authors,
+    page,
+    per,
+    count,
+    pagination,
+    search,
+  };
+};
+
+async function upsert(event: RequestEvent, isUpdate: boolean) {
+  const formData = await event.request.formData();
+  const id = formData.get('id');
+  const name = formData.get('name');
+
+  if (isUpdate && (typeof id !== 'string' || !id)) {
+    return fail(400, { message: 'Invalid id' });
+  }
+
+  // check name
+  if (typeof name !== 'string' || !name) {
+    return fail(400, { message: 'Invalid name' });
+  }
+
+  try {
+    if (isUpdate) {
+      await db
+        .update(schema.author)
+        .set({ name })
+        .where(eq(schema.author.id, id));
+    }
+    else {
+      await db
+        .insert(schema.author)
+        .values({ name });
+    }
+  }
+  catch (e) {
+    console.error(e);
+    return fail(500, { message: 'An error has occurred' });
+  }
+
+  return { message: isUpdate ? 'Author updated!' : 'Author created!' };
+}
+
+export const actions: Actions = {
+
+  create: async (event) => {
+    return upsert(event, false);
+  },
+  update: async (event) => {
+    return upsert(event, true);
+  },
+};
---
+++ apps/admin/src/routes/authors/+page.svelte
@@ -0,0 +1,129 @@
+<script lang='ts'>
+  import type { ActionData, PageServerData } from './$types';
+  import { enhance } from '$app/forms';
+  import { Table } from '$lib/components';
+  import { Input } from '@shared/components';
+  import { format } from '@std/datetime';
+
+  const { data, form }: { data: PageServerData;form: ActionData } = $props();
+  const authorAttrs: {
+    id?: string;
+    name: string;
+  } = $state({
+    id: undefined,
+    name: '',
+  });
+  let isUpdate = $state(false);
+  let checked = $state(false);
+  function drawerOpen(author: Partial<typeof data.authors[number]>) {
+    checked = true;
+    isUpdate = !!author.id;
+    authorAttrs.id = author.id;
+    authorAttrs.name = author.name ?? '';
+    if (form) {
+      form.message = '';
+    }
+  }
+</script>
+
+<div class='drawer drawer-end'>
+  <input id='drawer' type='checkbox' class='drawer-toggle' bind:checked />
+  <div class='drawer-content'>
+
+    <section class='m-auto w-full'>
+      <div class='mb-4 mt-8 flex'>
+        <h2 class='my-0 mr-8 block'>Authors</h2>
+        {#if data.isSuper}
+          <button class='block btn btn-primary drawer-button' onclick={() => {
+            drawerOpen({});
+          }}>Create author</button>
+        {/if}
+      </div>
+
+      <div class='flex space-x-2'>
+        <div class='text-sm'>Total: {data.count}</div>
+        <label for='modal' class='btn btn-sm'>
+          <span class='i-octicon-filter-16'></span>
+          Filter
+        </label>
+      </div>
+
+      {#if data.authors.length === 0}
+        <div class='my-8 w-full text-center'>No authors found!</div>
+      {/if}
+      <Table headers={[
+        { display: 'Name', sort: 'name' },
+        { display: 'Created at' },
+        { display: 'Updated at' },
+        { display: data.isSuper ? 'Action' : '' },
+      ]} pagination={data.pagination}>
+        {#snippet tbody()}
+          {#each data.authors as author}
+            <tr class='hover:bg-gray-100 dark:hover:bg-neutral-700'>
+              <td>{author.name}</td>
+              <td>
+                {format(author.createdAt, 'yyyy-MM-dd')}<br>
+                {format(author.createdAt, 'HH:mm:ss')}
+              </td>
+              <td>
+                {format(author.updatedAt, 'yyyy-MM-dd')}<br>
+                {format(author.updatedAt, 'HH:mm:ss')}
+              </td>
+              {#if data.isSuper}
+                <td>
+                  <button class='btn btn-circle btn-outline drawer-button btn-sm'
+                          aria-label='edit author'
+                          onclick={() => { drawerOpen(author); }}>
+                    <span class='i-octicon-pencil'></span>
+                  </button>
+                </td>
+              {/if}
+            </tr>
+          {/each}
+        {/snippet}
+      </Table>
+      <div>
+        'Create at' is local time
+      </div>
+    </section>
+
+  </div>
+  <div class='drawer-side'>
+    <label for='drawer' aria-label='close drawer' class='drawer-overlay'></label>
+    <div class='text-base-content min-h-full w-80 bg-gray-200 p-4 menu'>
+      <div>
+        <label for='drawer' class='btn btn-outline drawer-button'>close</label>
+      </div>
+      <!-- TODO: implement force logout -->
+      <form class='space-y-2' method='post' action={isUpdate ? '?/update' : '?/create'} use:enhance>
+        <div class='mb-4 text-center'>
+          <h2 class='my-0'>{isUpdate ? 'Update Author' : 'Create Author'}</h2>
+        </div>
+        <input type='hidden' name='id' bind:value={authorAttrs.id} />
+        <Input required name='name' bind:value={authorAttrs.name} placeholder='Name' icon='i-fluent-people-edit-16-regular' />
+        <button class='w-full btn btn-primary'>{isUpdate ? 'Update Author' : 'Create Author'}</button>
+        <p style='color: red'>{form?.message ?? ''}</p>
+      </form>
+    </div>
+  </div>
+</div>
+
+<input type='checkbox' id='modal' class='modal-toggle' />
+<div class='modal' role='dialog'>
+  <div class='modal-box'>
+    <h3 class='not-prose text-lg font-bold'>Filter</h3>
+    <!-- filters -->
+    <form class='space-y-2' method='get' action='?'>
+      <div class='flex space-x-2'>
+        <Input type='search' name='search' wrapperClass='w-full'
+               placeholder='Search by Name'
+               bind:value={data.search} />
+      </div>
+      <div class='modal-action'>
+        <label for='modal' class='btn'>Close</label>
+        <button class='btn btn-primary'>Search</button>
+      </div>
+    </form>
+  </div>
+  <label class='modal-backdrop' for='modal'>Close</label>
+</div>
