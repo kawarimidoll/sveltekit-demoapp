ab5e7f37bfbf3f689dc5be3990e924e6f522b28e
kawarimidoll <kawarimidoll+git@gmail.com>
2024-12-28 22:34:06 +1100

    feat: add action button

--- apps/admin/src/routes/authors/columns.ts
+++ apps/admin/src/routes/authors/columns.ts
@@ -1,13 +1,10 @@
 import type { schema } from '@shared/db';
 import type { ColumnDef } from '@tanstack/table-core';
-
+import { renderComponent } from '$lib/components/ui/data-table/index.js';
 import { format } from '@std/datetime';
+import DataTableActions from './data-table-actions.svelte';
 
 export const columns: ColumnDef<typeof schema.author.$inferSelect>[] = [
-  // Name
-  // Description
-  // Created at
-  // Updated at
   {
     accessorKey: 'name',
     header: 'Name',
@@ -25,5 +22,12 @@ export const columns: ColumnDef<typeof schema.author.$inferSelect>[] = [
     accessorKey: 'updatedAt',
     header: 'Updated at',
     cell: ({ row }) => format(row.getValue('updatedAt'), 'yyyy-MM-dd HH:mm:ss'),
+  },
+  {
+    header: 'Actions',
+    cell: ({ row }) => {
+      // You can pass whatever you need from `row.original` to the component
+      return renderComponent(DataTableActions, { id: row.original.id });
+    },
   },
 ];
---
+++ apps/admin/src/routes/authors/data-table-actions.svelte
@@ -0,0 +1,33 @@
+<script lang='ts'>
+  import { Button } from '$lib/components/ui/button/index.js';
+  import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+
+  const { id }: { id: string } = $props();
+</script>
+
+<DropdownMenu.Root>
+  <DropdownMenu.Trigger>
+    {#snippet child({ props })}
+      <Button
+        {...props}
+        variant='ghost'
+        size='icon'
+        class='relative size-8 p-0'
+      >
+        <span class='sr-only'>Open menu</span>
+        <div class='i-lucide-ellipsis size-4'></div>
+      </Button>
+    {/snippet}
+  </DropdownMenu.Trigger>
+  <DropdownMenu.Content>
+    <DropdownMenu.Group>
+      <DropdownMenu.GroupHeading>Actions</DropdownMenu.GroupHeading>
+      <DropdownMenu.Item onclick={() => navigator.clipboard.writeText(id)}>
+        Copy ID
+      </DropdownMenu.Item>
+    </DropdownMenu.Group>
+    <DropdownMenu.Separator />
+    <DropdownMenu.Item>Edit author</DropdownMenu.Item>
+    <DropdownMenu.Item>View books</DropdownMenu.Item>
+  </DropdownMenu.Content>
+</DropdownMenu.Root>
