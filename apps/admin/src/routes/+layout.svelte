d84b61bb574ca01a1fc706780c587d373972f7d2
kawarimidoll <kawarimidoll+git@gmail.com>
2024-12-28 15:55:41 +1100

    feat: add site-header

--- apps/admin/src/routes/+layout.svelte
+++ apps/admin/src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang='ts'>
+  import SiteHeader from '$lib/components/site-header.svelte';
   import { i18n } from '$lib/i18n';
   import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 
@@ -9,15 +10,7 @@
 </script>
 
 <ParaglideJS {i18n}>
-  <div class='h-12 w-full border-b'>
-    <a class='text-xl btn btn-ghost' href='/'>Admin Top</a>
-    <a class='link' href='/books'>Books</a>
-    <a class='link' href='/admins'>Admins</a>
-    <a class='link' href='/users'>Users</a>
-    <a class='link' href='/authors'>Authors</a>
-    <a class='link' href='/publishers'>Publishers</a>
-    <a class='link' href='/genres'>Genres</a>
-  </div>
+  <SiteHeader />
   <div class='mx-auto prose container'>
     {@render children()}
   </div>
