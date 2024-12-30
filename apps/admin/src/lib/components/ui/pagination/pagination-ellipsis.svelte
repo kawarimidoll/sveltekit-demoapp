7572053a716f4c21ac68b896f3da9075ae5c2eeb
kawarimidoll <kawarimidoll+git@gmail.com>
2024-12-30 15:27:08 +1100

    feat: add shadcn ui components

---
+++ apps/admin/src/lib/components/ui/pagination/pagination-ellipsis.svelte
@@ -0,0 +1,21 @@
+<script lang='ts'>
+  import type { WithElementRef, WithoutChildren } from 'bits-ui';
+  import type { HTMLAttributes } from 'svelte/elements';
+  import { cn } from '$lib/utils.js';
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    ...restProps
+  }: WithoutChildren<WithElementRef<HTMLAttributes<HTMLSpanElement>>> = $props();
+</script>
+
+<span
+  bind:this={ref}
+  aria-hidden='true'
+  class={cn('flex size-9 items-center justify-center', className)}
+  {...restProps}
+>
+  <span class='i-lucide-ellipsis size-4'></span>
+  <span class='sr-only'>More pages</span>
+</span>
---
+++ apps/admin/src/lib/components/ui/pagination/pagination-prev-button.svelte
@@ -0,0 +1,30 @@
+<script lang='ts'>
+  import { buttonVariants } from '$lib/components/ui/button';
+  import { cn } from '$lib/utils.js';
+  import { Pagination as PaginationPrimitive } from 'bits-ui';
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    children,
+    ...restProps
+  }: PaginationPrimitive.PrevButtonProps = $props();
+</script>
+
+{#snippet Fallback()}
+  <span class='i-lucide-chevron-left size-4'></span>
+  <span>Previous</span>
+{/snippet}
+
+<PaginationPrimitive.PrevButton
+  bind:ref
+  class={cn(
+    buttonVariants({
+      variant: 'ghost',
+      class: 'gap-1 pl-2.5',
+    }),
+    className,
+  )}
+  children={children || Fallback}
+  {...restProps}
+/>
