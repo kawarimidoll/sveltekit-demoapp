0c2aa848b0967d4c80d3dfff3208ee8f2ddecd84
kawarimidoll <kawarimidoll+git@gmail.com>
2024-12-31 13:12:06 +1100

    feat: author update dialog

--- apps/admin/src/routes/authors/columns.ts
+++ apps/admin/src/routes/authors/columns.ts
@@ -1,81 +1,85 @@
 import type { schema } from '@shared/db';
 import type { ColumnDef } from '@tanstack/table-core';
+import type { SuperValidated } from 'sveltekit-superforms';
 import { renderComponent } from '$lib/components/ui/data-table/index.js';
 import { format } from '@std/datetime';
 import DataTableActions from './data-table-actions.svelte';
 import DataTableCheckbox from './data-table-checkbox.svelte';
 import DataTableColumnHeader from './data-table-column-header.svelte';
 
-export const columns: ColumnDef<typeof schema.author.$inferSelect>[] = [
-  {
-    id: 'select',
-    header: ({ table }) =>
-      renderComponent(DataTableCheckbox, {
-        'checked': table.getIsAllPageRowsSelected(),
-        'indeterminate':
-          table.getIsSomePageRowsSelected()
-          && !table.getIsAllPageRowsSelected(),
-        'onCheckedChange': value => table.toggleAllPageRowsSelected(!!value),
-        'controlledChecked': true,
-        'aria-label': 'Select all',
-        'class': 'translate-y-[2px]',
-      }),
-    cell: ({ row }) =>
-      renderComponent(DataTableCheckbox, {
-        'checked': row.getIsSelected(),
-        'onCheckedChange': value => row.toggleSelected(!!value),
-        'controlledChecked': true,
-        'aria-label': 'Select row',
-        'class': 'translate-y-[2px]',
-      }),
-    enableSorting: false,
-    enableHiding: false,
-  },
-  {
-    accessorKey: 'name',
-    header: ({ column }) => {
-      return renderComponent(DataTableColumnHeader, {
-        column,
-        title: 'Name',
-      });
+export function genColumns(
+  form: SuperValidated,
+): ColumnDef<typeof schema.author.$inferSelect>[] {
+  return [
+    {
+      id: 'select',
+      header: ({ table }) =>
+        renderComponent(DataTableCheckbox, {
+          'checked': table.getIsAllPageRowsSelected(),
+          'indeterminate': table.getIsSomePageRowsSelected()
+            && !table.getIsAllPageRowsSelected(),
+          'onCheckedChange': value => table.toggleAllPageRowsSelected(!!value),
+          'controlledChecked': true,
+          'aria-label': 'Select all',
+          'class': 'translate-y-[2px]',
+        }),
+      cell: ({ row }) =>
+        renderComponent(DataTableCheckbox, {
+          'checked': row.getIsSelected(),
+          'onCheckedChange': value => row.toggleSelected(!!value),
+          'controlledChecked': true,
+          'aria-label': 'Select row',
+          'class': 'translate-y-[2px]',
+        }),
+      enableSorting: false,
+      enableHiding: false,
     },
-  },
-  {
-    accessorKey: 'description',
-    header: ({ column }) => {
-      return renderComponent(DataTableColumnHeader, {
-        column,
-        title: 'Description',
-      });
+    {
+      accessorKey: 'name',
+      header: ({ column }) => {
+        return renderComponent(DataTableColumnHeader, {
+          column,
+          title: 'Name',
+        });
+      },
     },
-  },
-  {
-    accessorKey: 'createdAt',
-    header: ({ column }) => {
-      return renderComponent(DataTableColumnHeader, {
-        column,
-        title: 'Created at',
-      });
+    {
+      accessorKey: 'description',
+      header: ({ column }) => {
+        return renderComponent(DataTableColumnHeader, {
+          column,
+          title: 'Description',
+        });
+      },
     },
-    cell: ({ row }) => format(row.getValue('createdAt'), 'yyyy-MM-dd HH:mm:ss'),
-  },
-  {
-    accessorKey: 'updatedAt',
-    header: ({ column }) => {
-      return renderComponent(DataTableColumnHeader, {
-        column,
-        title: 'Updated at',
-      });
+    {
+      accessorKey: 'createdAt',
+      header: ({ column }) => {
+        return renderComponent(DataTableColumnHeader, {
+          column,
+          title: 'Created at',
+        });
+      },
+      cell: ({ row }) => format(row.getValue('createdAt'), 'yyyy-MM-dd HH:mm:ss'),
     },
-    cell: ({ row }) => format(row.getValue('updatedAt'), 'yyyy-MM-dd HH:mm:ss'),
-  },
-  {
-    id: 'actions',
-    cell: ({ row }) => {
-      // You can pass whatever you need from `row.original` to the component
-      return renderComponent(DataTableActions, { id: row.original.id });
+    {
+      accessorKey: 'updatedAt',
+      header: ({ column }) => {
+        return renderComponent(DataTableColumnHeader, {
+          column,
+          title: 'Updated at',
+        });
+      },
+      cell: ({ row }) => format(row.getValue('updatedAt'), 'yyyy-MM-dd HH:mm:ss'),
     },
-    enableSorting: false,
-    enableHiding: false,
-  },
-];
+    {
+      id: 'actions',
+      cell: ({ row }) => {
+        // You can pass whatever you need from `row.original` to the component
+        return renderComponent(DataTableActions, { form, data: row.original });
+      },
+      enableSorting: false,
+      enableHiding: false,
+    },
+  ];
+}
--- apps/admin/src/routes/authors/data-table-actions.svelte
+++ apps/admin/src/routes/authors/data-table-actions.svelte
@@ -1,12 +1,37 @@
 <script lang='ts'>
+  import type { schema } from '@shared/db';
   import { Button } from '$lib/components/ui/button';
+  import * as Dialog from '$lib/components/ui/dialog';
   import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+  import * as Form from '$lib/components/ui/form';
+  import { Input } from '$lib/components/ui/input';
   import { toast } from 'svelte-sonner';
+  import { type Infer, superForm, type SuperValidated } from 'sveltekit-superforms';
+  import { zodClient } from 'sveltekit-superforms/adapters';
+  import { updateSchema, type UpdateSchema } from './schema';
 
-  const { id }: { id: string } = $props();
+  type Props = {
+    form: SuperValidated<Infer<UpdateSchema>>;
+    data: typeof schema.author.$inferSelect;
+  };
+  const { form: formSrc, data }: Props = $props();
+
+  const form = superForm(formSrc, {
+    validators: zodClient(updateSchema),
+  });
+
+  const { form: formData, enhance } = form;
+
   function copyId() {
-    navigator.clipboard.writeText(id);
+    navigator.clipboard.writeText(data.id);
     toast.success('ID copied to clipboard');
+  }
+
+  let dialogOpen = $state(false);
+
+  function open() {
+    formData.set(data);
+    dialogOpen = true;
   }
 </script>
 
@@ -32,7 +57,46 @@
       </DropdownMenu.Item>
     </DropdownMenu.Group>
     <DropdownMenu.Separator />
-    <DropdownMenu.Item>Edit author</DropdownMenu.Item>
+    <DropdownMenu.Item onclick={open}>
+      Edit
+    </DropdownMenu.Item>
     <DropdownMenu.Item>View books</DropdownMenu.Item>
   </DropdownMenu.Content>
 </DropdownMenu.Root>
+
+<Dialog.Root bind:open={dialogOpen}>
+  <Dialog.Content>
+    <Dialog.Header>
+      <Dialog.Title>Edit author</Dialog.Title>
+      <Dialog.Description>
+        Make changes to author here. Click save when you're done.
+      </Dialog.Description>
+    </Dialog.Header>
+    <form method='POST' action='?/update' use:enhance>
+      <input type='hidden' name='id' bind:value={$formData.id} />
+      <Form.Field {form} name='name'>
+        <Form.Control>
+          {#snippet children({ props })}
+            <Form.Label>Name</Form.Label>
+            <Input {...props} bind:value={$formData.name} />
+          {/snippet}
+        </Form.Control>
+        <Form.Description>This is author's display name.</Form.Description>
+        <Form.FieldErrors />
+      </Form.Field>
+      <Form.Field {form} name='description'>
+        <Form.Control>
+          {#snippet children({ props })}
+            <Form.Label>Description</Form.Label>
+            <Input {...props} bind:value={$formData.description} />
+          {/snippet}
+        </Form.Control>
+        <Form.Description>This is author's description.</Form.Description>
+        <Form.FieldErrors />
+      </Form.Field>
+      <Dialog.Footer>
+        <Form.Button>Save changes</Form.Button>
+      </Dialog.Footer>
+    </form>
+  </Dialog.Content>
+</Dialog.Root>
