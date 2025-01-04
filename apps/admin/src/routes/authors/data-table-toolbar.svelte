96a2addb3d430332c7271bc5133c7601d3f164df
kawarimidoll <kawarimidoll+git@gmail.com>
2025-01-04 14:18:27 +1100

    feat: include reset button in Input component

--- apps/admin/src/routes/authors/data-table-toolbar.svelte
+++ apps/admin/src/routes/authors/data-table-toolbar.svelte
@@ -1,13 +1,16 @@
 <script lang='ts' generics='TData'>
   import type { Table } from '@tanstack/table-core';
-  import { Button, buttonVariants } from '$lib/components/ui/button';
+  import { buttonVariants } from '$lib/components/ui/button';
   import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
   import { Input } from '$lib/components/ui/input';
 
   const { table }: { table: Table<TData> } = $props();
-  const isFiltered = $derived(table.getState().columnFilters.length > 0);
 
   let globalFilterValue = $state('');
+  $effect(() => {
+    table.setGlobalFilter(globalFilterValue);
+    return () => '';
+  });
   table.setGlobalFilter('');
 </script>
 
@@ -16,21 +19,9 @@
     <Input
       placeholder='Filter...'
       bind:value={globalFilterValue}
-      onkeyup={(e) => {
-        table.setGlobalFilter(String(e.currentTarget.value));
-      }}
       class='h-8 max-w-sm'
+      resetIf={() => globalFilterValue !== ''}
     />
-    {#if isFiltered}
-      <Button
-        variant='ghost'
-        onclick={() => table.resetColumnFilters()}
-        class='h-8 px-2 lg:px-3'
-      >
-        Reset
-        <span class='i-lucide-x'></span>
-      </Button>
-    {/if}
   </div>
   <DropdownMenu.Root>
     <DropdownMenu.Trigger
